97c6842e9e9da51fcf5546798b3ef0f58abb1260
rawizy <rawastoluwani1@gmail.com>
2024-12-12 18:20:43 +0100

    Deployed Website

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDw3Q2-A5uiSMRywzOn8bYZ3s9nA7r7BEQ",
-    authDomain: "laims-bakery-and-pastry.firebaseapp.com",
-    databaseURL: "https://laims-bakery-and-pastry-default-rtdb.firebaseio.com",
-    projectId: "laims-bakery-and-pastry",
-    storageBucket: "laims-bakery-and-pastry.firebasestorage.app",
-    messagingSenderId: "488480581743",
-    appId: "1:488480581743:web:abb03d755fdc33469e059a",
-    measurementId: "G-PSYLBK48MN"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
